b3a907e94d8689f9569c0cfc612b1eb9a9013d2b
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2025-08-17 05:53:23 -0300

    feat: create component ui base

---
+++ src/app/_components/ui-base/ui-base.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-base',
+  imports: [],
+  templateUrl: './ui-base.component.html',
+  styleUrl: './ui-base.component.css'
+})
+export class UiBaseComponent {
+
+}
