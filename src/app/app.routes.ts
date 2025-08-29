ff3be34950de6b25b82cbf21e80210a5cd304634
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2025-08-29 02:38:33 -0300

    feat: define initial routing for certificates and related components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,4 +3,17 @@ import { CertificatesComponent } from './pages/certificates/certificates.compone
 import { CertificateFormComponent } from './pages/certificate-form/certificate-form.component';
 import { CertificatePageComponent } from './pages/certificate-page/certificate-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: CertificatesComponent
+    },
+    {
+        path: 'certificates/new',
+        component: CertificateFormComponent
+    },
+    {
+        path: 'certificates/:id',
+        component: CertificatePageComponent
+    }
+];
