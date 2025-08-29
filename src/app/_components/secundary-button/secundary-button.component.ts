3026f7234e157aa5a701befb9a5087ae714f9dd7
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2025-08-29 02:45:16 -0300

    fix: remove unused RouterLink import from secundary-button component

--- src/app/_components/secundary-button/secundary-button.component.ts
+++ src/app/_components/secundary-button/secundary-button.component.ts
@@ -1,14 +1,9 @@
 import { Component, Input } from '@angular/core';
-<<<<<<< HEAD
-import { CommonModule } from '@angular/common';
-=======
 import { CommonModule } from '@angular/common';;
-import { RouterLink } from '@angular/router';
->>>>>>> 9862d0326d2c1c8db020e005869edeb230fcc1b2
 
 @Component({
     selector: 'app-secundary-button',
-    imports: [CommonModule, RouterLink],
+    imports: [CommonModule],
     templateUrl: './secundary-button.component.html',
 })
 export class SecundaryButtonComponent {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { UiBaseComponent } from './_components/ui-base/ui-base.component';
-<<<<<<< HEAD
 import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     imports: [NavbarComponent, UiBaseComponent, RouterOutlet],
-=======
-import { RouterOutlet} from '@angular/router';
-
-@Component({
-    selector: 'app-root',
-    imports: [
-    NavbarComponent,
-    UiBaseComponent,
-    RouterOutlet
-],
->>>>>>> 9862d0326d2c1c8db020e005869edeb230fcc1b2
     templateUrl: './app.component.html',
 })
 export class AppComponent {
